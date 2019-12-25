ad30e8ccdc7b0411c9506bd7ac3db8571b6d1139
IamMorgan <bugidi002@users.noreply.github.com>
2019-12-25 12:34:26 +0300

    todo 25.12

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import store from './components/redux/reduxStore';
+import store from './redux/reduxStore';
 
 ReactDOM.render(<App store={store}/>, document.getElementById('root'));
 
